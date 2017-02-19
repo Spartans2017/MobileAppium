376785d8ad2d12f14b9857695c9a9828611bf147
michaltrunek <michal.trunek@tipico.com>
2017-02-19 21:13:06 +0100

    helper method for reusable methods as click, asserts added

--- src/test/methods.java
+++ src/test/methods.java
@@ -1,11 +1,14 @@
 package test;
 
 import io.appium.java_client.android.AndroidDriver;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.URL;
+import java.util.Objects;
+import java.util.function.BooleanSupplier;
 
 /**
  * Helper test.methods
@@ -20,8 +23,46 @@ public class methods {
 
     }
 
+    private static WebElement element(By by) {
+        return driver.findElement(by);
+    }
+
     public static void click(By by) {
-        WebElement button = driver.findElement(by);
-        button.click();
+        element(by).click();
+    }
+
+    public static void cleanup() {
+        driver.quit();
+    }
+
+    private static Object getAtt(By by, String attribute) {
+
+        return element(by).getAttribute(attribute);
+
+    }
+
+    private static String getText(By by) {
+
+        return element(by).getText();
+    }
+
+    private static Boolean isEnabled(By by) {
+        return element(by).isEnabled();
+
+    }
+
+    public static void assertAttribute(String description, By by, String attribute, String expected) {
+        Object actualAttribute = getAtt(by, attribute);
+        Assert.assertEquals(description, expected, actualAttribute);
+    }
+
+    public static void assertText(String description, By by, String expected) {
+        String actualText = getText(by);
+        Assert.assertEquals(description, expected, actualText);
+    }
+
+    public static void assertEnabled(String description, By by, Boolean expected) {
+        Boolean actual = isEnabled(by);
+        Assert.assertEquals(description, expected, actual);
     }
 }
