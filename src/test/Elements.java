99d7f44dd9a4b0927eb22b86234af15f776b426f
michaltrunek <michal.trunek@tipico.com>
2017-02-19 21:53:10 +0100

    elements set as static final as they are not changing further in the project

--- src/test/Elements.java
+++ src/test/Elements.java
@@ -4,8 +4,8 @@ import org.openqa.selenium.By;
 
 public class Elements {
 
-    static By addButton = By.id("com.test.calc:id/add_b");
-    static By subButton = By.id("com.test.calc:id/sub_b");
-    static By output = By.id("com.test.calc:id/tv2");
-    static By title = By.id("android:id/title");
+    static final By addButton = By.id("com.test.calc:id/add_b");
+    static final By subButton = By.id("com.test.calc:id/sub_b");
+    static final By output = By.id("com.test.calc:id/tv2");
+    static final By title = By.id("android:id/title");
 }
