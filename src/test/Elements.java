6718f8edd4c880af7dd3938aa47e97543464b205
michaltrunek <michal.trunek@tipico.com>
2017-02-19 21:27:18 +0100

    Local elements class Elements created

---
+++ src/test/Elements.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.openqa.selenium.By;
+
+public class Elements {
+
+    static By addButton = By.id("com.test.calc:id/add_b");
+    static By subButton = By.id("com.test.calc:id/sub_b");
+    static By output = By.id("com.test.calc:id/tv2");
+    static By title = By.id("android:id/title");
+}
