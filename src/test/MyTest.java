2ac9d6ae6f6b9fdc95c250eaed7d3f72531e8a02
michaltrunek <michal.trunek@tipico.com>
2017-02-19 21:13:32 +0100

    Test cases added

--- src/test/MyTest.java
+++ src/test/MyTest.java
@@ -1,102 +1,69 @@
 package test;
 
-import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
-import java.net.URL;
-
 
 public class MyTest {
 
-    AndroidDriver driver;
     @Before
     public void setUp() throws Exception {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("deviceName", "virtual_device_android");
 
-        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        methods.init(capabilities);
     }
 
     @After
     public void tearDown() throws Exception{
-        driver.quit();
+        methods.cleanup();
     }
 
     @Test
     public void layout(){
-        WebElement addButton = driver.findElement(By.id("com.test.calc:id/add_b"));
-        WebElement subButton = driver.findElement(By.id("com.test.calc:id/sub_b"));
-        WebElement title = driver.findElement(By.id("android:id/title"));
-        WebElement output = driver.findElement(By.id("com.test.calc:id/tv2"));
-
-        Boolean addButtonEnabled = addButton.isEnabled();
-        Boolean subButtonEnabled = subButton.isEnabled();
-        String outputNotClickable = output.getAttribute("clickable");
-        String titleText = title.getText();
-        Assert.assertEquals("addition button should be enabled", true, addButtonEnabled);
-        Assert.assertEquals("subtraction button should be enabled", true, subButtonEnabled);
-        Assert.assertEquals("output should not be clickable", "false", outputNotClickable);
-        Assert.assertEquals("title should equal 'Calculator'", "Calculator", titleText);
+        methods.assertEnabled("addition button should be enabled", By.id("com.test.calc:id/add_b"), true);
+        methods.assertEnabled("subtraction button should be enabled", By.id("com.test.calc:id/sub_b"), true);
+        methods.assertAttribute("output should not be clickable", By.id("com.test.calc:id/tv2"), "clickable", "false");
+        methods.assertText("title should equal 'Calculator'", By.id("android:id/title"), "Calculator");
     }
 
     @Test
     public void additionOperation(){
-        WebElement button = driver.findElement(By.id("com.test.calc:id/add_b"));
-        String buttonText = button.getText();
-        Assert.assertEquals("should equals to 'ADD'", "ADD", buttonText);
-        button.click();
+        methods.click(By.id("com.test.calc:id/add_b"));
+        methods.assertText("should equals to 'ADD'", By.id("com.test.calc:id/add_b"), "ADD");
     }
 
     @Test
     public void subtractionOperation(){
-        WebElement button = driver.findElement(By.id("com.test.calc:id/sub_b"));
-        String buttonText = button.getText();
-        Assert.assertEquals("should equals to 'SUB'", "SUB", buttonText);
-        button.click();
+        methods.click(By.id("com.test.calc:id/sub_b"));
+        methods.assertText("should equals to 'SUB'", By.id("com.test.calc:id/sub_b"), "SUB");
     }
 
     @Test
     public void multipleOperationsSameButton(){
-        WebElement addButton = driver.findElement(By.id("com.test.calc:id/add_b"));
-        WebElement output = driver.findElement(By.id("com.test.calc:id/tv2"));
 
-        addButton.click();
-        String output1 = output.getText();
-        Assert.assertEquals("'new number =1' value should be displayed in the output text field", "new number =1", output1);
-        addButton.click();
-        String output2 = output.getText();
-        Assert.assertEquals("'new number =2' value should be displayed in the output text field", "new number =2", output2);
-        addButton.click();
-        String output3 = output.getText();
-        Assert.assertEquals("'new number =3' value should be displayed in the output text field", "new number =3", output3);
+        methods.click(By.id("com.test.calc:id/add_b"));
+        methods.assertText("'new number =1' value should be displayed in the output text field", By.id("com.test.calc:id/tv2"), "new number =1");
+        methods.click(By.id("com.test.calc:id/add_b"));
+        methods.assertText("'new number =2' value should be displayed in the output text field", By.id("com.test.calc:id/tv2"), "new number =2");
+        methods.click(By.id("com.test.calc:id/add_b"));
+        methods.assertText("'new number =3' value should be displayed in the output text field", By.id("com.test.calc:id/tv2"), "new number =3");
     }
 
     @Test
     public void multipleOperationsRandom(){
-        WebElement addButton = driver.findElement(By.id("com.test.calc:id/add_b"));
-        WebElement subButton = driver.findElement(By.id("com.test.calc:id/sub_b"));
-        WebElement output = driver.findElement(By.id("com.test.calc:id/tv2"));
 
-        subButton.click();
-        String output1 = output.getText();
-        Assert.assertEquals("'new number =-1' value should be displayed in the output text field", "new number =-1", output1);
-        addButton.click();
-        String output2 = output.getText();
-        Assert.assertEquals("'new number =0' value should be displayed in the output text field", "new number =0", output2);
-        subButton.click();
-        String output3 = output.getText();
-        Assert.assertEquals("'new number =-1' value should be displayed in the output text field", "new number =-1", output3);
-        addButton.click();
-        String output4 = output.getText();
-        Assert.assertEquals("'new number =0' value should be displayed in the output text field", "new number =0", output4);
-        addButton.click();
-        String output5 = output.getText();
-        Assert.assertEquals("'new number =1' value should be displayed in the output text field", "new number =1", output5);
+        methods.click(By.id("com.test.calc:id/sub_b"));
+        methods.assertText("'new number =-1' value should be displayed in the output text field", By.id("com.test.calc:id/tv2"), "new number =-1");
+        methods.click(By.id("com.test.calc:id/add_b"));
+        methods.assertText("'new number =0' value should be displayed in the output text field", By.id("com.test.calc:id/tv2"), "new number =0");
+        methods.click(By.id("com.test.calc:id/sub_b"));
+        methods.assertText("'new number =-1' value should be displayed in the output text field", By.id("com.test.calc:id/tv2"), "new number =-1");
+        methods.click(By.id("com.test.calc:id/add_b"));
+        methods.assertText("'new number =0' value should be displayed in the output text field", By.id("com.test.calc:id/tv2"), "new number =0");
+        methods.click(By.id("com.test.calc:id/add_b"));
+        methods.assertText("'new number =1' value should be displayed in the output text field", By.id("com.test.calc:id/tv2"), "new number =1");
     }
 }
